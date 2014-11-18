3d7321ec16ef042931d45f8cf2df6a466d71103b
LiviaMiura <livataliba@gmail.com>
2014-11-18 14:40:44 -0200

    Funcionando Integrado

--- src/main/java/br/leona/servidor/service/ObservacaoService.java
+++ src/main/java/br/leona/servidor/service/ObservacaoService.java
@@ -5,7 +5,18 @@ import br.leona.servidor.mocks.PantIlt;
 public class ObservacaoService {
 
     public int movimentar(PantIlt p, String l) {
-        return 1;
+        try { // Call Web Service Operation
+            br.leona.estacao.controller.ControllerServices_Service service = new br.leona.estacao.controller.ControllerServices_Service();
+            br.leona.estacao.controller.ControllerServices port = service.getControllerServicesPort();
+            // TODO initialize WS operation arguments here
+            java.lang.String graus = "10";
+            // TODO process result here
+            int result = port.moverEsquerda(graus);
+            System.out.println("Result = "+result);
+            return result;
+        } catch (Exception ex) {
+            return 0;
+        }
     }
     
 }
